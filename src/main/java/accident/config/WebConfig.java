9dbe51cf3b32c28719f89e41f30c3f377f49781b
Sergey Manannikov <s_manannikov@mail.ru>
2021-04-02 20:31:56 +0300

    Spring MVC

---
+++ src/main/java/accident/config/WebConfig.java
@@ -0,0 +1,22 @@
+package accident.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@ComponentScan("accident")
+public class WebConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("./WEB-INF/view/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+}
---
+++ src/main/java/accident/controller/IndexControl.java
@@ -0,0 +1,13 @@
+package accident.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexControl {
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
